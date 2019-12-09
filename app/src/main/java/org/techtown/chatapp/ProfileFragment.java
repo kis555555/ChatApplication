7f7e0c94467243547254e3cf6ef367cba239bd9b
kis2702 <52072748+kis2702@users.noreply.github.com>
2019-12-09 23:55:55 +0900

    ChatApp

--- app/src/main/java/org/techtown/chatapp/ProfileFragment.java
+++ app/src/main/java/org/techtown/chatapp/ProfileFragment.java
@@ -1,24 +1,59 @@
 package org.techtown.chatapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 
-
-public class ProfileFragment extends Fragment {
-
+public class ProfileFragment extends Fragment
+{
+    ImageView ivUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ivUser = (ImageView) v.findViewById(R.id.ivUser);
+        ivUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 1);
+            }
+        });
+
+        return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        Uri image = data.getData();
+        try
+        {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),image);
+            ivUser.setImageBitmap(bitmap);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
